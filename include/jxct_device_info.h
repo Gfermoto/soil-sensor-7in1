f8fe27b7f635fd9d445097fea173211cffadaa6e
user <user@example.com>
2025-06-10 12:55:19 +0300

    Обновлена версия до 2.0

--- include/jxct_device_info.h
+++ include/jxct_device_info.h
@@ -2,7 +2,7 @@
 
 static const char DEVICE_MANUFACTURER[] = "Eyera";
 static const char DEVICE_MODEL[] = "JXCT-7in1";
-static const char DEVICE_SW_VERSION[] = "1.8";
+static const char DEVICE_SW_VERSION[] = "2.0";
 
 #include <Arduino.h>
 
