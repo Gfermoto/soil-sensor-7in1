d4419745f2ea6eca12f5b9fde0c70f227299a874
user <user@example.com>
2025-07-03 22:45:13 +0300

    Восстановлена Doxygen документация API: полная генерация в site/api/html/

---
+++ site/api/html/csrf__protection_8cpp.js
@@ -0,0 +1,12 @@
+var csrf__protection_8cpp =
+[
+    [ "checkCSRFSafety", "csrf__protection_8cpp.html#a0320c55f4de318df92de8a73e8e7d828", null ],
+    [ "generateCSRFToken", "csrf__protection_8cpp.html#ab27a975e89338b0b23a40e028b1ae9b2", null ],
+    [ "getCSRFHiddenField", "csrf__protection_8cpp.html#aab8be89322026594573b8f3c8064bbbc", null ],
+    [ "initCSRFProtection", "csrf__protection_8cpp.html#ac922ac6444b8b990e13e3746ca931065", null ],
+    [ "methodToString", "csrf__protection_8cpp.html#ad9d33eff9bf83f09111cc522049930bc", null ],
+    [ "validateCSRFToken", "csrf__protection_8cpp.html#a936ecd18be81531b306c2c4705f7e860", null ],
+    [ "CSRF_TOKEN_LIFETIME", "csrf__protection_8cpp.html#a0857d7f29ac875e2258e9bd3487e6ce0", null ],
+    [ "currentCSRFToken", "csrf__protection_8cpp.html#a235cd7fd2255ab7b1afb6437529776f8", null ],
+    [ "tokenGeneratedTime", "csrf__protection_8cpp.html#a7eb48d2dc5c72caad5b9064030e8ce5e", null ]
+];
---
+++ site/api/html/fake__sensor_8cpp.js
@@ -0,0 +1,5 @@
+var fake__sensor_8cpp =
+[
+    [ "fakeSensorTask", "fake__sensor_8cpp.html#a88a6670c876eb66b3358db30715c4416", null ],
+    [ "startFakeSensorTask", "fake__sensor_8cpp.html#abd277e18147efb1103e91eac01627612", null ]
+];
